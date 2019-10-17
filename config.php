9a6948b1759220c0d1d96e28808bf2cf2143ac77
Mikkycody <horluwatowbeey@gmail.com>
2019-10-17 16:53:52 +0100

    Initial commit

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.wowonder.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com   
+// +------------------------------------------------------------------------+
+// | WoWonder - The Ultimate PHP Social Networking Platform
+// | Copyright (c) 2016 WoWonder. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "root";//uhmdakgp_swingle
+// MySQL Database Password
+$sql_db_pass = "";//Newport231193
+// MySQL Database Name
+$sql_db_name = "swingle";//uhmdakgp_swingle
+
+// Site URL
+$site_url = "http://localhost/swingle"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "176B-7XJG-TTLE-TAEH"; // Your purchase code, don't give it to anyone. 
+?>
